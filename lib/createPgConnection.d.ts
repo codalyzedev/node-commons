562953fd43b2b9a0ec2974e04e7e47151cd8421e
Naisheel Verdhan <nverdhan@gmail.com>
2020-01-28 04:05:20 +0530

    bump up pg-promise version

--- lib/createPgConnection.d.ts
+++ lib/createPgConnection.d.ts
@@ -1,3 +1,3 @@
 import * as pgPromise from 'pg-promise';
-declare const _default: (databaseUrl: string, disableMonitor?: boolean | undefined) => pgPromise.IDatabase<unknown>;
+declare const _default: (databaseUrl: string, disableMonitor?: boolean | undefined) => pgPromise.IDatabase<{}, import("pg-promise/typescript/pg-subset").IClient>;
 export default _default;
