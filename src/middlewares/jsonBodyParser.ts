5c2f28365508db48481b9f8e866b015709b8cd97
UJPrasad <jyothiprasadu@gmail.com>
2020-06-05 22:24:51 +0530

    increase body parser limit

--- src/middlewares/jsonBodyParser.ts
+++ src/middlewares/jsonBodyParser.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
 export default function (req: express.Request, res: express.Response, next: express.NextFunction) {
-  bodyParser.json()(req, res, err => {
+  bodyParser.json({ limit: '10mb' })(req, res, err => {
     if (err instanceof Error) next(new Error('INVALID_JSON_BODY_PARSER'));
     else next();
   });
